6786ea13a3340bf620c0db91cda7bbc226f16307
nikita-qwerty123 <nikitos.barca@gmail.com>
2023-11-25 15:10:48 +0200

    updatehm6

--- hm6.js
+++ hm6.js
@@ -1,3 +1,4 @@
+//hm1
 function drawTriangle(height, symbol) {
     for (let i = 1; i <= height; i++) {
         let row = '';
@@ -7,11 +8,25 @@ function drawTriangle(height, symbol) {
         console.log(row);
     }
 }
-// drawTriangle(7, '&');
-let sum=0;
-for (let i=1; i<=100; i++) {
-    if (i%2) continue;
-    sum+=i;
-}
-console.log(sum);
+drawTriangle(7, '\\_/');
 
+//hm2
+let sum = 0;
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 !== 0) {
+        sum += i;
+    }
+}
+
+console.log("Сума чисел від 1 до 100 (не кратних 3): " + sum);
+
+//hm3 
+function pow(a,b) {
+    let result=a;
+    for (let i=1; i<b; i++) {
+    result*= a;
+    }
+    return console.log(result);
+}
+pow(3,4);
